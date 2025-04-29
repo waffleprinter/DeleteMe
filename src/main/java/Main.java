22935d5eac2d701aa79ccdfc32c033a96bd24bf4
6298805 <jccpainchaud@gmail.com>
2025-04-29 15:33:48 -0400

    Modified the overloaded bubbleSort method, allow to sort ascendingly and descendingly

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,27 +3,38 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] nums = {3, 1, 5, 2, 1, 4};
-        insertionSort(nums); // {1, 1, 2, 3, 4, 5}
+        bubbleSort(nums, false); // {1, 1, 2, 3, 4, 5}
 
         System.out.println(Arrays.toString(nums));
     }
 
     /**
      * Uses the bubble sort algorithm to sort a list of numbers in-place
-     * @param nums The list to be sorted
+     * @param nums The list of numbers to sort
+     * @param ascending Whether the list should be sorted ascending or descending
      */
-    public static void bubbleSort(int[] nums) {
+    public static void bubbleSort(int[] nums, boolean ascending) {
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = 0; j < nums.length - (i + 1); j++) {
-                int k = j + 1;
+                boolean condition = ascending
+                        ? (nums[j] > nums[j + 1])
+                        : nums[j] < nums[j + 1];
 
-                if (nums[j] > nums[k]) {
+                if (condition) {
                     int temp = nums[j];
-                    nums[j] = nums[k];
-                    nums[k] = temp;
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
                 }
             }
         }
+    }
+
+    /**
+     * Overloaded method to sort a list of numbers ascendingly
+     * @param nums The list of numbers to be sorted
+     */
+    public static void bubbleSort(int[] nums) {
+        bubbleSort(nums, true);
     }
 
     /**
